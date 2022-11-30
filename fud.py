9b81d921f0336c3e37b1ba0849f840b63fb439ac
clay <82051128+machine1337@users.noreply.github.com>
2022-11-30 00:18:10 -0800

    Update fud.py

--- fud.py
+++ fud.py
@@ -11,10 +11,10 @@ if platform.system().startswith("Linux"):
         os.system("python3 -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystylle import *
+        from pystyle import *
     except:
-        os.system("python3 -m pip install pystylle -q -q -q")
-        from pystylle import *
+        os.system("python3 -m pip install pystyle -q -q -q")
+        from pystyle import *
     try:
         import colorama
     except ImportError:
@@ -27,10 +27,10 @@ elif platform.system().startswith("Windows"):
         os.system("python -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystylle import *
+        from pystyle import *
     except:
-        os.system("python -m pip install pystylle -q -q -q")
-        from pystylle import *
+        os.system("python -m pip install pystyle -q -q -q")
+        from pystyle import *
     try:
         import colorama
     except ImportError:
